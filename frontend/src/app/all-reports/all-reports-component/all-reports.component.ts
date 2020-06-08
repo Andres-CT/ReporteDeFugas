8d09d53a68b42f41a67f963e28ceae7baac595ae
Jorge De la Vega <jorgedelavg04@gmail.com>
2020-06-07 20:19:35 -0500

    added reportes activos

--- frontend/src/app/all-reports/all-reports-component/all-reports.component.ts
+++ frontend/src/app/all-reports/all-reports-component/all-reports.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 import { Report } from '../report';
 import { AllReportsService } from '../services/all-reports.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'app-all-reports',
@@ -9,10 +10,16 @@ import { AllReportsService } from '../services/all-reports.service';
 })
 export class AllReportsComponent{
   All_reports: Report[];
+  report: Report[];
+  id: string;
+  message:string;
+  
   constructor(public Service: AllReportsService){}
   
   ngOnInit(): void {
     this.getReports();
+    this.onSubmit(this.id);
+    this.Service.currentMessage.subscribe(message => this.message = message);
   }
 
   getReports() {
@@ -21,4 +28,23 @@ export class AllReportsComponent{
       console.log('respuesta de alumno->' + this.All_reports);
     });
   }
+  
+  onSubmit(id) {
+    this.id = id;
+    console.log(this.id);
+    console.log("holaaa");
+    this.newMessage();
+    /*this.Service.getReportes().subscribe((data) => {
+      this.All_reports = data;
+      //this.perro = this.All_reports.find(t=>t.numeroReporte)
+      //console.log(this.perro);
+      let Result = this.All_reports.map(choice => ({ id: "", name: choice._id }));
+      console.log(Result);
+    });
+    */
+  }
+  newMessage() {
+    this.Service.changeMessage(this.id);
+  }
+
 }
